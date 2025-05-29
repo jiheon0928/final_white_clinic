14ecd265104237477a8a14f921cdcc9e36ddc318
jiheon <wwlgjs77@gmail.com>
2025-05-29 14:14:48 +0900

    ..

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
