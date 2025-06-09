85a00a532f88675ad346f2324b9c4fa806dde3ab
GN-Lee <rhkdsud0917@gmail.com>
2025-06-10 00:09:54 +0900

    test7

--- frontend/admin_browser/app/rider/update/page.tsx
+++ frontend/admin_browser/app/rider/update/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import { RiderUpdate } from "@/components/riderList/riderUpdate/RiderUpdate";
 
 const UpdatePage = () => {
-  return <RiderUpdate />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RiderUpdate />
+    </Suspense>
+  );
 };
 
 export default UpdatePage;
